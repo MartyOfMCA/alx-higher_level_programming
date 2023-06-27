64e33493b4db5e072b77997011dddb08fec5c1e0
Martin <martyofmca@gmail.com>
2023-06-27 09:23:29 -0700

    add a Shape class with a size attribute - 0x06

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+Define a module for the Square shape
+"""
+
+
+class Square:
+    """
+    Define the Square shape
+    """
+    def __init__(self, size):
+        """
+        Initialize an instance of the square shape
+        with the given size
+        """
+        self.__size = size
