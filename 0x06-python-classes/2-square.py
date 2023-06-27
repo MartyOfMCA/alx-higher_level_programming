abc1e692b7c0ed9b73adfe0a90c39ca07fbab928
Martin <martyofmca@gmail.com>
2023-06-27 10:14:00 -0700

    initialize size attribute in Square class - 0x06

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+Define a module for the Square shape
+"""
+
+
+class Square:
+    """
+    Define the Square shape
+    """
+    def __init__(self, size=0):
+        if (type(size) is int):
+            if (size < 0):
+                raise ValueError("size must be >= 0")
+            self.__size = size
+        else:
+            raise TypeError("size must be an integer")
