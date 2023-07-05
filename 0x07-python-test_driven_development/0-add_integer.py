2e1e2cf67b3fc32f89e2458f82641b82f4f4311a
Martin <martyofmca@gmail.com>
2023-07-04 17:56:14 -0700

    add function to calculate sum with test cases - 0x07

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+This module defines the add_integer function
+>>> add_integer(3, 3)
+6
+"""
+
+
+def add_integer(num1, num2=98):
+    """
+    Returns the sum of the two given numbers as
+    an integer.
+
+    Parameters
+    num1 : integer / float
+        The first number
+    num2 : integer / float, optional
+        The second number. Default value is 98
+
+    Raises
+    TypeError
+        When any of the given parameters is not
+        either an integer or a float
+
+    Return : The sum of the two given numbers as an 
+    integer
+    """
+
+    message = None
+
+    if (type(num1) not in(int, float) or type(num2) not in (int, float)):
+        if (type(num1) not in (int, float)):
+            message = "a must be an integer"
+        else:
+            message = "b must be an integer"
+        raise TypeError(message)
+
+    return (int(num1) + int(num2))
