1d22f47d1d8518fae0461f7d7d1b9f8090683a1e
Martin <martyofmca@gmail.com>
2023-07-10 18:19:49 -0700

    declare area method for geometry objects - 0x0A

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Defines the BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """
+    The Geometry class
+    """
+    def area(self):
+        """
+        Returns the area for the geometry object.
+        """
+        raise Exception("area() is not implemented")
