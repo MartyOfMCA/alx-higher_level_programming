d043e6f8ffff7e44fc646949d67617734acd1cf6
Martin <martyofmca@gmail.com>
2023-07-17 18:45:13 -0700

    add Square class with test class - 0x0C

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" This module defines the Square class """
+from .rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ Represents the square shape """
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Initialize an instance of a square
+
+        Parameters
+        size : integer
+            The size (width and height) for the square
+        x : integer, optional
+            The x axis for displaying the square
+        y : integer, optional
+            The y axis for displaying the square
+        """
+        super().__init__(id=id, x=x, y=y, width=size, height=size)
+
+    def __str__(self):
+        """
+        Returns the string representation of the square.
+        """
+        return ("[Square] ({}) {}/{} - {}".format(self.id, self.x,
+                self.y, self.width, self.height))
---
+++ 0x0C-python-almost_a_circle/tests/test_square.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+""" This module defines a test class for the Square class """
+import unittest
+from models.square import Square
+
+
+class TestSquare(unittest.TestCase):
+
+    def test_width_from_valid_values_with_id(self):
+        square = Square(10, 1, 1, 10)
+        self.assertEqual(10, square.width)
+
+    def test_height_from_valid_values_with_id(self):
+        square = Square(10, 1, 1, 10)
+        self.assertEqual(10, square.height)
+
+    def test_x_from_valid_values_with_id(self):
+        square = Square(10, 1, 1, 10)
+        self.assertEqual(1, square.x)
+
+    def test_y_from_valid_values_with_id(self):
+        square = Square(10, 1, 1, 10)
+        self.assertEqual(1, square.y)
+
+    def test_id_from_valid_values_with_id(self):
+        square = Square(10, 1, 1, 10)
+        self.assertEqual(10, square.id)
+
+    def test_width_from_invalid_type(self):
+        with self.assertRaises(TypeError):
+            Square("10", 1, 1, 10)
+    
+    def test_x_from_invalid_x(self):
+        with self.assertRaises(TypeError):
+            Square(10, "1", 1, 10)
+
+    def test_y_from_invalid_y(self):
+        with self.assertRaises(TypeError):
+            Square(10, 1, "1", 10)
+
+    def test_id_without_obj_id(self):
+        square = Square(10, 1, 1)
+        self.assertEqual(20, square.id)
+
+    def test_id_from_invalid_value(self):
+        with self.assertRaises(ValueError):
+            Square(-10, 1, 1, 10)
+
+    def test_width_from_negative_value(self):
+        with self.assertRaises(ValueError):
+            Square(0, 1, 1, 10)
+
+    def test_x_from_invalid_value(self):
+        with self.assertRaises(ValueError):
+            Square(10, -1, 1, 10)
+
+    def test_y_from_invalid_value(self):
+        with self.assertRaises(ValueError):
+            Square(10, 1, -1, 10)
+
+    def test_height_one_positional_arg_provided(self):
+        square = Square(10)
+        self.assertEqual(10, square.height)
+
+    def test_no_positional_arg_provided(self):
+        with self.assertRaises(TypeError):
+            Square()
