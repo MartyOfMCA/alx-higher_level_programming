3b9e3be6dfdef99b84d414824b8d89d2dc17b3ea
Martin <martyofmca@gmail.com>
2023-08-16 18:21:08 -0700

    add a table with foreign key constraint - 0x0E

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,15 @@
+-- Create the database hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Connect to the newly created database
+CONNECT hbtn_0d_usa;
+
+-- Create a table names cities in the current
+-- database
+CREATE TABLE IF NOT EXISTS cities
+(
+	id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	FOREIGN KEY(state_id) REFERENCES states(id)
+);
