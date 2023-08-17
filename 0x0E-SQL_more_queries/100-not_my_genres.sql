e740fb21442f5e3571e6d0ad1368267dc9d6176a
Martin <martyofmca@gmail.com>
2023-08-16 21:55:46 -0700

    retrieves all shows not for a specific genre - 0x0E

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,13 @@
+-- Retrieves all genres for the show
+-- called Dexter
+SELECT	name
+FROM	tv_genres
+WHERE	id NOT IN
+	(
+		SELECT	genre_id
+		FROM	tv_show_genres
+			INNER JOIN tv_shows
+				ON id = show_id
+		WHERE	title = 'Dexter'
+	)
+ORDER BY name;
