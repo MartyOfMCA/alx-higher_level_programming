504f5d21319bd0c39cb261f43c5181ad7030f1e5
Martin Amankwaa <martyofmca@gmail.com>
2023-09-11 16:28:18 -0700

    print factorial for command-line argument - 0x12

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+function factorial (number) {
+  if (number === 1) {
+    return (1);
+  }
+  return (number * factorial(number - 1));
+}
+
+const number = parseInt(process.argv[2]);
+
+if (isNaN(number)) {
+  console.log(1);
+} else {
+  console.log(factorial(number));
+}
