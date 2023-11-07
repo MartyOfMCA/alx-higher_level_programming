3bd550349ba24b0a7c6baf14fccf6926443d2024
Martin <martyofmca@gmail.com>
2023-11-07 10:12:00 +0000

    change element color after DOM loads completely with vanilla JS - 0x15

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,8 @@
+/*
+ * Update the text color of the header element
+ * to red using vanilla JS only after the page
+ * is fully loaded.
+ */
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('header').style.color = '#FF0000';
+});
